898b255373999b22e4a8b361f33d55fa7d97ca0d
SNunesA <snunesadiers@gmail.com>
2025-03-21 21:29:21 -0300

    player

--- RPG/client.py
+++ RPG/client.py
@@ -1,16 +1,21 @@
 import time
-from factory import create_npc, NPCType
+from factory import create_npc, NPCType, create_player, PlayerType
 #executar
 #python3 RPG/client.py 
 
 # Criando os personagens
 dragon = create_npc(NPCType.Dragon)
 mage=create_npc(NPCType.Mage)
+
+mario = create_player(PlayerType.Mario)
+joker = create_player(PlayerType.Joker)
+
 #mage = Mage()
 #dragon = Dragon()
 
 # Loop da batalha até que um deles morra
 while dragon.saude > 0 and mage.saude > 0:
+    
     dragon.atacar(mage)  # Dragon ataca Mage
     if mage.saude == 0:
         print(f"\n{mage.nome} morreu! {dragon.nome} venceu!")
--- RPG/factory.py
+++ RPG/factory.py
@@ -1,5 +1,5 @@
 from models import (
-    NPC, Dragon, Mage
+    NPC, Dragon, Mage, Player
 )
 from enum import Enum #pra enumerar os tipos
 
@@ -16,3 +16,17 @@ def create_npc(npc_type: NPCType) -> NPC:
     else:
         #caso o cliente passar um tipo desconhecido ao factory
         raise ValueError("Unknown type: {npc_type}")
+    
+
+class PlayerType(Enum):
+    Mario = "Mario"
+    Joker = "Joker"
+
+def create_player(player_type: PlayerType) -> Player:
+    if player_type.Mario.value == player_type.value: 
+        return Mario() #retorna um instanciamento do tipo Dragon
+    elif player_type.Joker.value == player_type.value: 
+        return Joker() #valor padrao(default) pra qualidade
+    else:
+        #caso o cliente passar um tipo desconhecido ao factory
+        raise ValueError("Unknown type: {player_type}")
