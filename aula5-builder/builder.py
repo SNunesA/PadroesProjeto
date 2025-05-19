7fcce755e136ed23b85ee36b3b6df2afa03e0ccf
SNunesA <snunesadiers@gmail.com>
2025-05-19 04:41:03 -0300

    atv

--- aula5-builder/builder.py
+++ aula5-builder/builder.py
@@ -1,11 +1,11 @@
 class Notebook:
-    def __init__(self, gpu=None):
+    def __init__(self, gpu="RTX"):
         self.gpu: str = gpu # possibilita inicializar a gpu default
         self.cpu = None
         self.ram = None
         self.ssd = None
         self.tela = None
-    def __repr__(self):
+    def __repr__(self): #representaçao
         return f"""Notebook: {id(self)} \n
         modelo: {self.cpu} \n
         gpu: {self.gpu} \n
@@ -15,11 +15,11 @@ class NoteBuilder:
     def __init__(self):
         self.notebook = Notebook()
 
-    def add_gpu(self, gpu: str):
+    def set_gpu(self, gpu: str):
         self.notebook.gpu = gpu
         return self
     
-    def add_ram(self, ram: str):
+    def set_ram(self, ram: str):
         self.notebook.ram = ram
         return self
     
@@ -33,8 +33,8 @@ class NoteBuilder:
 if __name__ == "__main__":
     # usuario seleciona as opcoes
     notebook = (NoteBuilder()
-                .add_gpu("RTX 4090")
-                .add_ram("32GB")
+                .set_gpu("RTX 4090")
+                .set_ram("32GB")
                 .set_cpu("Intel")
                 .build()
                 )
@@ -47,7 +47,7 @@ if __name__ == "__main__":
 
 """
     builder = NoteBuilder()
-    builder.add_gpu()
-    .add_ram("32GB")
+    builder.set_gpu()
+    .set_ram("32GB")
     .build()
 """
