71c3a4a75276d17a69f3fbb8077de835ff5da26e
SNunesA <snunesadiers@gmail.com>
2025-06-06 21:41:57 -0300

    projeto

---
+++ ProjetoFinal/client.py
@@ -0,0 +1,35 @@
+import json
+import os
+from models import (NPC,NPCAgua, NPCFogo, MageAgua, DragonAgua,MageFogo,DragonFogo)
+from abc import ABC,abstractmethod
+class AbstractFactory(ABC):
+    def createDragon(self)-> NPC:
+        pass
+    def createMage(self)-> NPC:
+        pass
+#ELEMENTOS
+class aguaFactory(AbstractFactory):
+    def createDragon() -> NPCAgua:
+        return DragonAgua()
+    def createMage() -> NPCAgua:
+        return MageAgua()
+    def createNPCAgua(tipo):
+        if tipo =='Dragon':
+            return aguaFactory.createDragon()
+        # if tipo == 'Mage':
+
+
+
+json_file=open('ProjetoFinal/arquivo.json', mode="r")
+sistema=json.load(json_file)
+json_file.close()
+
+
+for c in sistema["NPCS"]:
+    if c['elemento'] == "agua":
+        npc1=aguaFactory.createNPCAgua(c['nome'])
+    
+print(npc1)
+
+        
+    
