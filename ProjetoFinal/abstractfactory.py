68bc8356fff06209fbcdb2474f2122a73757c000
Aluno <aluno-lab@ifpr.com>
2025-06-12 20:59:34 -0300

    feat: passagem de parametros para o factory

---
+++ ProjetoFinal/abstractfactory.py
@@ -0,0 +1,23 @@
+from models import (NPC,NPCAgua, NPCFogo, MageAgua, DragonAgua,MageFogo,DragonFogo)
+from abc import ABC,abstractmethod
+
+class AbstractFactory(ABC):
+    @abstractmethod
+    def createNPC(self) -> NPC:
+        pass
+#ELEMENTOS
+class aguaFactory(AbstractFactory):
+    def createNPC(dic) -> NPCAgua:
+        params = {k: dic[k] for k in ("nome", "saude", "ataque", "defesa")}
+        if dic['nome'] =='Dragon':
+            return DragonAgua(**params)
+        if dic['nome'] == 'Mage':
+            return MageAgua(**params)
+        
+class fogoFactory(AbstractFactory):
+    def createNPC(dic) -> NPCFogo:
+        params = {k: dic[k] for k in ("nome", "saude", "ataque", "defesa")}
+        if dic['nome'] =='Dragon':
+            return DragonFogo(**params)
+        if dic['nome'] == 'Mage':
+            return MageFogo(**params)
