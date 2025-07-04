c6e169a2b82802c9658370a0f503a530f9bdf084
SNunesA <snunesadiers@gmail.com>
2025-07-03 23:40:55 -0300

    fix: atribuiçao retirada do status

--- ProjetoFinal/decoratorequip.py
+++ ProjetoFinal/decoratorequip.py
@@ -8,8 +8,6 @@ class EquipamentoDecorator(Player):
 
     @property
     def player(self) -> Player:
-    
-
         return self._player
     @property
     def status(self):
@@ -19,30 +17,36 @@ class EquipamentoDecorator(Player):
         return self._player.operation()
 
 class Vida(EquipamentoDecorator):
+    def __init__(self, player: Player):
+        super().__init__(player)
+        self._player.saude += 10
     def operation(self) -> str:
         print(1)
         return f"PoçãoVida_Equipado({self.player.operation()})"
     @property
     def status(self):
         # Acessando diretamente atributos do objeto decorado
-        self._player.saude+=10
         return self._player.__str__(equip='Poção de Vida')
 
 
 class Ataque(EquipamentoDecorator):
+    def __init__(self, player: Player):
+        super().__init__(player)
+        self._player.ataque+=10
     def operation(self) -> str:
         print(1)
         return f"PoçãoAtaque_Equipada({self.player.operation()})"
     @property
     def status(self):
-        self._player.ataque+=10
         return self._player.__str__(equip='Poção de Ataque')
 
 class Defesa(EquipamentoDecorator):
+    def __init__(self, player: Player):
+        super().__init__(player)
+        self._player.defesa+=10
     def operation(self) -> str:
         print(1)
         return f"PoçãoDefesa_Equipada({self.player.operation()})"
     @property
     def status(self):
-        self._player.defesa+=10
         return self._player.__str__(equip='Poção de Defesa')
